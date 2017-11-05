3344849c8ecbc9a5585467c6148df533175a1244
Jesus Arredondo <chuyarredondo@gmail.com>
2017-11-04 20:45:29 -0600

    Add the scripture app

---
+++ Scripture-Application/dbo.Scriptures.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Scriptures]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [book] NCHAR(10) NULL, 
+    [chapter] NCHAR(10) NULL, 
+    [verse] NCHAR(10) NULL, 
+    [text] NCHAR(10) NULL
+)
